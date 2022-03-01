1250869f18f2dfafcdff80dc57c3a2394b7dcaa9
Gabriel Pagella <gabrielp@gmail.com>
2022-03-01 00:31:56 -0300

    Set active profile to local in GorviLedgerApplicationTests

--- src/test/kotlin/com/gorvi/ledger/GorviLedgerApplicationTests.kt
+++ src/test/kotlin/com/gorvi/ledger/GorviLedgerApplicationTests.kt
@@ -2,8 +2,10 @@ package com.gorvi.ledger
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("local")
 class GorviLedgerApplicationTests {
 
 	@Test
