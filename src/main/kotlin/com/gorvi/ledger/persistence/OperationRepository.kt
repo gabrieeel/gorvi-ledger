c9a44b197fb5cc0207253f3df5e84b69271fec17
Gabriel Pagella <gabrielp@gmail.com>
2022-04-06 20:26:48 -0300

    Repository method to find purchase operations by currency

--- src/main/kotlin/com/gorvi/ledger/persistence/OperationRepository.kt
+++ src/main/kotlin/com/gorvi/ledger/persistence/OperationRepository.kt
@@ -1,9 +1,20 @@
 package com.gorvi.ledger.persistence
 
+import com.gorvi.ledger.model.Currency
 import com.gorvi.ledger.model.Operation
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
-interface OperationRepository : CrudRepository<Operation, Long>{
+interface OperationRepository : CrudRepository<Operation, Long> {
+
     fun findAllByOrderByCreatedDesc() : List<Operation>
 
+    @Query("select distinct o from Operation as o" +
+            "   join o.movements as m" +
+            "   join m.balance as b" +
+            "   where o.type='PURCHASE'" +
+            "   and m.amountIn > 0" +
+            "   and b.currency = :currency" +
+            "   order by o.created desc")
+    fun findPurchaseOperationsByCurrency(currency: Currency) : List<Operation>
 }
